1442864fdefdf336506176e8811aac8be2ef35d9
Hall Liu <hallliu000@gmail.com>
2014-01-15 20:58:50 -0600

    stuff

---
+++ cs230/hw1/fw.h
@@ -0,0 +1,6 @@
+#ifndef FW_H
+#define FW_H
+
+int *fw_parallel(int *, int, int);
+int *fw_serial(int *, int);
+#endif
--- cs230/hw1/fw_serial.c
+++ cs230/hw1/fw_serial.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include "fw.h"
 
 int main(int argc, char **argv) {
     printf("stub\n");
 }
 
-int *fw_parallel(int *adj, int n_nodes, int n_threads) {
+int *fw_serial(int *adj, int n_nodes) {
     printf("stub\n");
     return NULL;
 }
