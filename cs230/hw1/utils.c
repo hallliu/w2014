0e8c02d85f4d408da1ff9ed9a5d7484045882ce2
Hall Liu <hallliu000@gmail.com>
2014-01-17 17:06:00 -0600

    utils

---
+++ cs230/hw1/utils.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include "utils.h"
+
+/* Grabs an int matrix from the named csv file and loads it
+ * into the int array. Assumes that the csv file contains a
+ * well-formed matrix and has a terminating , or newline.
+ */
+void from_csv(int *matrix, char *filename, int n) {
+    FILE f;
+    if (filename != NULL) {
+        f = fopen(filename, "r");
+    } else {
+        f = stdin;
+    }
+
+    int matr_ctr = 0, int num_ctr = 0;
+    char *num_buf = malloc (9); // Longest number possible has 8 characters
+    while (!feof(f)) {
+        num_buf[num_ctr] = fgetc(f);
+        if (num_buf[num_ctr] == ',' || num_buf[num_ctr] == '\n') {
+            num_buf[num_ctr] = '\0';
+            matrix[matr_ctr] = (int) strtol (num_buf);
+            num_ctr = 0;
+            matr_ctr++;
+        } else {
+            num_ctr++;
+        }
+    }
+    assert (matr_ctr == n * n);
+    free (num_buf);
+    if (f != stdin) {
+        fclose (f);
+    }
+    return;
+}
+
+/* Takes the int matrix and prints it to a csv file. If filename is null, print to stdout
+ */
+void to_csv(int *matrix, char *filename, int n) {
+    FILE f;
+    if (filename != NULL) {
+        f = fopen(filename, "w");
+    } else {
+        f = stdout;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            fprintf(f, "%d,", matrix[i * n + j]);
+        }
+        fprintf (f, "%d\n", matrix[i * n + j]);
+    }
+    if (f != stdout) {
+        fclose (f);
+    }
+    return;
+}
---
+++ cs230/hw1/utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void from_csv(int *, char *, int);
+void to_csv(int *, char *, int);
+#endif
