9f2d26c6881c349bffdf797b83536525c916b668
Hall Liu <hallliu000@gmail.com>
2014-01-18 16:21:33 -0600

    runner script

--- cs230/hw1/run.sh
+++ cs230/hw1/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -r build
-rm wrapper.so
+git clone git@github.com:hallliu/w2014.git
+cd w2014/cs230/hw1/
 
 ./setup.py build_ext -i
 ./perf.py
@@ -11,3 +11,5 @@ rm wrapper.so
 
 ./setup.py build_ext -i --time_wait
 ./perf.py time_wait
+
+tar -zcvf results.tar.gz results
