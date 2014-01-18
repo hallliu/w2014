dbdf7d9d2ef562f81114ae8e360182ce4c94085f
Hall Liu <hallliu000@gmail.com>
2014-01-17 20:45:24 -0600

    unit test mod

--- cs230/hw1/fw.c
+++ cs230/hw1/fw.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 #include <unistd.h>
 #include "fw.h"
+#include "utils.h"
 
 /* Assumptions on arguments: -n must be present, the number of nodes.
  * -t may or may not be present. If it is, then it's # threads. If not,
@@ -10,8 +12,9 @@
  */
 int main(int argc, char *argv[]) {
     int n_nodes = 0, n_threads = 0;
+    char *filename, *outfile = NULL;
     int opt;
-    while (opt = getopt(argc, argv, "n:t:"), opt != -1) {
+    while (opt = getopt(argc, argv, "n:t:o:"), opt != -1) {
         switch (opt) {
             case 'n':
                 n_nodes = (int) strtol(argv[optind]);
@@ -19,18 +22,27 @@ int main(int argc, char *argv[]) {
             case 't':
                 n_threads = (int) strtol(argv[optind]);
                 break;
+            case 'o':
+                outfile = strcpy(argv[optind])
         }
     }
-    char *filename;
     if (optind >= argc) {
         filename = NULL;
     }
     else {
-        char *filename = argv[optind];
+        filename = argv[optind];
     }
     /* Initialize adj matrix and fill it in */
     int *adj_matrix = malloc (n_nodes * n_nodes * sizeof(int));
+    int *result;
     from_csv(adj_matrix, filename, n_nodes);
+    if (n_threads == 0) {
+        result = fw_serial(adj_matrix, n_nodes);
+    } else {
+        result = fw_parallel(adj_matrix, n_nodes, n_threads);
+    }
+    to_csv(result, outfile, n_nodes);
+    return 0;
 }
 
 int *fw_parallel(int *adj, int n_nodes, int n_threads) {
--- cs230/hw1/test.py
+++ cs230/hw1/test.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python2
 import wrapper
 import numpy as np
+import unittest
 
-def test_load_csv():
-    filename = '/tmp/TEST_LOAD_CSV_hallliu.csv'
-    a = np.random.randint(10000000, size=(40,40))
-    b = np.empty((40,40), dtype='int32')
-    np.savetxt(filename, a, delimiter=',', fmt='%d')
-    wrapper.from_csv(filename, b, 40)
-    assert np.all(a == b)
+class UtilTests(unittest.TestCase):
+    def test_load_csv(self):
+        filename = '/tmp/TEST_LOAD_CSV_hallliu.csv'
+        a = np.random.randint(10000000, size=(40,40))
+        b = np.empty((40,40), dtype='int32')
+        np.savetxt(filename, a, delimiter=',', fmt='%d')
+        wrapper.from_csv(filename, b, 40)
+        self.assertTrue(np.all(a == b))
+    
+    def test_dump_csv(self):
+        filename = '/tmp/TEST_DUMP_CSV_hallliu.csv'
+        a = np.random.randint(10000000, size=(40,40)).astype('int32')
+        wrapper.to_csv(filename, a, 40)
+        b = np.loadtxt(filename, delimiter=',', dtype='int32')
+        self.assertTrue(np.all(a == b))
 
-def test_dump_csv():
-    filename = '/tmp/TEST_DUMP_CSV_hallliu.csv'
-    a = np.random.randint(10000000, size=(40,40)).astype('int32')
-    wrapper.to_csv(filename, a, 40)
-    b = np.loadtxt(filename, delimiter=',', dtype='int32')
-    assert np.all(a == b)
-
-test_load_csv()
-test_dump_csv()
+if __name__ == '__main__':
+    unittest.main()
