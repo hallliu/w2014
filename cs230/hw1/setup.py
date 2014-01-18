495045809d1f62267acc8380170641ea31073974
Hall Liu <hallliu000@gmail.com>
2014-01-18 13:42:26 -0600

    compile errors

--- cs230/hw1/setup.py
+++ cs230/hw1/setup.py
@@ -4,7 +4,7 @@ import numpy as np
  
 module1 = Extension('wrapper', 
         extra_compile_args = ['-Wall', '-Werror', '-g', '-std=gnu99'],
-        sources = ['wrapper.c', 'utils.c', 'fw.c'])
+        sources = ['wrapper.c', 'utils.c', 'fw.c', 'stopwatch.c'])
  
 setup (name = 'wrapper', 
         ext_modules = [module1],
