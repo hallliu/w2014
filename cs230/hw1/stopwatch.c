46de729ae652c441a77c91d63c36442c244bc8b8
Hall Liu <hallliu000@gmail.com>
2014-01-19 13:39:12 -0600

    oops

--- cs230/hw1/stopwatch.c
+++ cs230/hw1/stopwatch.c
@@ -11,6 +11,6 @@ void stopTimer(StopWatch_t * watch) {
 }
 
 double getElapsedTime(StopWatch_t * watch) {
-	return ((watch->stopTime.tv_sec-watch->startTime.tv_sec)*1000LL + watch->stopTime.tv_usec-watch->startTime.tv_usec)/1000.0f;
+	return ((watch->stopTime.tv_sec-watch->startTime.tv_sec)*1000000LL + watch->stopTime.tv_usec-watch->startTime.tv_usec)/1000.0f;
 }
 
