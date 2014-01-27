ab0e3dbf86ca5b6681d9b91c573a1ba47730efc3
Hall Liu <hallliu000@gmail.com>
2014-01-27 03:14:05 -0600

    s3

--- stat310/hw3_files/p2.m
+++ stat310/hw3_files/p2.m
@@ -1,4 +1,44 @@
-function p2()
+function x = p2(A, b)
+    x = ldlsolve(A, b);
 end
 
-function 
+function x = ldlsolve(A, b)
+    [L,D,p] = ldl(A, 'vector');
+    b_permut = b(p);
+    opts.LT = true;
+    
+    dltx = linsolve(L, b_permut, l_opts);
+    ltx = blk2_slv(D, dltx);
+    opts.TRANSA = true;
+    x = linsolve(L, ltx, opts);
+    
+    x(p) = x;
+end
+
+%{
+Solves a 2x2 block diagonal system
+%}
+function x = blk2_slv(D, b)
+    [n, ~] = size(D);
+    x = 1:n;
+    i = 1;
+    while i <= n
+        if i==n
+            x(i) = b(i) / D(i,i);
+            i = i + 1;
+            continue;
+        end
+            
+        if D(i,i+1) ~= 0
+            x(i:i+1) = D(i:i+1, i:i+1)\b(i:i+1);
+            i = i + 2;
+        else
+            x(i) = b(i) / D(i, i);
+            i = i + 1;
+        end
+    end
+end
+            
+            
+            
+            
