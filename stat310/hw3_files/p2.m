fb1062ae3a043671dd63dd378630b11579c36422
Hall Liu <hallliu000@gmail.com>
2014-01-28 22:06:34 -0600

    something

--- stat310/hw3_files/p2.m
+++ stat310/hw3_files/p2.m
@@ -1,6 +1,28 @@
-%poop
-function x = p2(A, b)
-    x = ldlsolve(A, b);
+%
+function x = p2(upto)
+
+    sizes = 25*2.^(1:upto);
+    sp_times = 1:upto;
+    d_times = 1:upto;
+    
+    for i=1:upto
+        W_sp = makeW(sizes(i), 1);
+        W_d = makeW(sizes(i), 0);
+        e = ones(sizes(i)*2, 1);
+        
+        tic;
+        ldlsolve(W_d, e);
+        d_times(i) = toc;
+        
+        tic;
+        x = ldlsolve(W_sp, e);
+        sp_times(i) = toc;
+    end
+    
+    d_coefs = polyfit(log(sizes), log(d_times), 1);
+    s_coefs = polyfit(log(sizes), log(sp_times), 1);
+    sprintf('Dense: C=%.3g, alpha=%.3f\n', exp(d_coefs(2)), d_coefs(1))
+    sprintf('Sparse: C=%.3g, alpha=%.3f\n', exp(s_coefs(2)), s_coefs(1))
 end
 
 function x = ldlsolve(A, b)
@@ -8,12 +30,19 @@ function x = ldlsolve(A, b)
     b = b(p);
     
     dltx = L\b;
-    ltx = D\dltx;
+    if issparse(A)
+        ltx = D\dltx;
+    else
+        ltx = blk2_slv(D, dltx).';
+    end
+    
     x = L.'\ltx;
     
     x(p) = x;
 end
 
+% This assumes a 2x2 block matrix in D and solves Dx=b. Appears to be
+% faster than doing D\b for dense D.
 function x = blk2_slv(D, b)
     [n, ~] = size(D);
     x = 1:n;
@@ -32,5 +61,16 @@ function x = blk2_slv(D, b)
             x(i) = b(i) / D(i, i);
             i = i + 1;
         end
+    end
+end
+
+function W = makeW(N, sp)
+    diag_entries = [ones(1, N), zeros(1, N)];
+    lap_tmp=2*diag(diag_entries)-diag(ones(2*N-1,1),1)-diag(ones(2*N-1,1),-1);
+    if sp
+        [i,j,s] = find(lap_tmp);
+        W = sparse(i, j, s, 2*N, 2*N);
+    else
+        W = lap_tmp;
     end
 end
