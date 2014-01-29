57f0fef98be50ec8f59d6d393e0309b0e9122380
Hall Liu <hallliu000@gmail.com>
2014-01-28 22:29:58 -0600

    better formatting

--- stat310/hw3_files/p2.m
+++ stat310/hw3_files/p2.m
@@ -1,6 +1,4 @@
-%
 function x = p2(upto)
-
     sizes = 25*2.^(1:upto);
     sp_times = 1:upto;
     d_times = 1:upto;
@@ -51,17 +49,14 @@ function x = blk2_slv(D, b)
     i = 1;
     while i <= n
         if i==n
-            x(i) = b(i) / D(i,i);
-            i = i + 1;
+            x(i) = b(i) / D(i,i); i = i + 1;
             continue;
         end
             
         if D(i,i+1) ~= 0
-            x(i:i+1) = D(i:i+1, i:i+1)\b(i:i+1);
-            i = i + 2;
+            x(i:i+1) = D(i:i+1, i:i+1)\b(i:i+1); i = i + 2;
         else
-            x(i) = b(i) / D(i, i);
-            i = i + 1;
+            x(i) = b(i) / D(i, i); i = i + 1;
         end
     end
 end
