7dc93502fa4c8d6fa0ddb06a42e65d9844adb597
Hall Liu <hallliu000@gmail.com>
2014-02-01 01:07:45 -0600

    helper c files

---
+++ cs230/hw2/tests/test_main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "test_queue.h"
+
+/*
+ * Invoke with the test name as the first arg (excluding test_)
+ * and additional arguments as subsequent
+ * args.
+ */
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf("Specify test name\n");
+        return 1;
+    }
+
+    if (!strcmp(argv[1], "queue_serial")) {
+        test_queue_serial(atoi(argv[2]));
+        return 0;
+    }
+
+    if (!strcmp(argv[1], "queue_parallel_1")) {
+        test_queue_parallel_1(atoi(argv[2], argv[3], argv[4]));
+        return 0;
+    }
+
+    if (!strcmp(argv[1], "queue_parallel_2")) {
+        test_queue_parallel_2(atoi(argv[2], argv[3], argv[4]));
+        return 0;
+    }
+
+    println("Invalid test name");
+    return 1;
+}
---
+++ cs230/hw2/tests/test_queue.h
@@ -0,0 +1,8 @@
+#ifndef TEST_QUEUE_H
+#define TEST_QUEUE_H
+
+void test_queue_serial(int);
+void test_queue_parallel_1(int, int, int);
+void test_queue_parallel_2(int, int, int);
+
+#endif
