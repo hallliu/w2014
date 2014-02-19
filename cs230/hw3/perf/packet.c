cc372f70f7e45de0660583f2d55713701ef35594
Hall Liu <hallliu000@gmail.com>
2014-02-19 15:48:56 -0600

    packet test compiles

--- cs230/hw3/perf/packet.c
+++ cs230/hw3/perf/packet.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/time.h>
-#include "../locks.h"
 #include "../Utils/stopwatch.h"
+#include "../Utils/generators.h"
+#include "../Utils/fingerprint.h"
+#include "../Utils/packetsource.h"
+#include "../locks.h"
 #include "../queue.h"
 #include "../packet_workers.h"
 
@@ -15,6 +18,8 @@
 #error "This file needs the tuned version of the backoff lock"
 #endif
 
+#define QUEUE_DEPTH 8
+
 static void usleep(int n) {
     struct timespec sleep_time = {.tv_sec = n / 1000000, .tv_nsec = (n % 1000000) * 1000l};
     nanosleep(&sleep_time, NULL);
@@ -24,6 +29,7 @@ void *alarm(void *_num_ms) {
     int *num_ms = (int *) _num_ms;
     usleep(*num_ms);
     *num_ms = 0;
+    return NULL;
 }
 
 void parallel_dispatcher 
@@ -58,11 +64,11 @@ void parallel_dispatcher
 
     struct thread_data *worker_data = calloc (n_src, sizeof(struct thread_data));
     pthread_t *workers = calloc (n_src, sizeof(pthread_t));
+
     for (int i = 0; i < n_src; i++) {
         worker_data[i].queues = queues;
         worker_data[i].n_queues = n_src;
         worker_data[i].home_queue = i;
-        worker_data[i].queue_hits = calloc (n_src, sizeof(int));
 
         pthread_create (workers + i, NULL, worker_fn, (void *) (worker_data + i));
     }
@@ -73,7 +79,7 @@ void parallel_dispatcher
     // the alarm thread sets it to 0 when time is up.
     volatile int not_done = n_ms;
     pthread_t alarm_thread;
-    pthread_create(alarm_thread, NULL, alarm, &not_done);
+    pthread_create(&alarm_thread, NULL, alarm, (void *) &not_done);
 
     while (not_done) {
         for (int i = 0; i < n_src; i++) {
