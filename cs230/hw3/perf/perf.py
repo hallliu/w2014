8b15953698e6b2bd2e3ae91b28d9b757ab61cad3
Hall Liu <hallliu000@gmail.com>
2014-02-19 16:31:07 -0600

    perf done

--- cs230/hw3/perf/perf.py
+++ cs230/hw3/perf/perf.py
@@ -44,7 +44,7 @@ def timeout_output(cmd, timeout):
         return out
 
 def _log(msg):
-    logfile.write('{0}      {1}'.format(datetime.datetime.utcnow(), msg))
+    logfile.write('{0}      {1}\n'.format(datetime.datetime.utcnow(), msg))
 
 def counter_overhead_time():
     locks_here = locks + ['noop']
@@ -105,7 +105,7 @@ def counter_scaling_work():
     return lock_tps
 
 def counter_fairness():
-    num_threads = 4 
+    num_threads = 16
     lock_tps = np.zeros(len(locks), dtype='float64')
     lock_stddevs = np.zeros(len(locks), dtype='float64')
 
@@ -113,8 +113,8 @@ def counter_fairness():
         for j in range(3):
             print('counter_fairness: lock {0} iteration {1}'.format(l, j))
             outs = timeout_output(['./perf_main', 'parallel_time', l, '2000', str(num_threads)], 3000).split()
-            lock_tps[i] += float(outs[-1])
-            lock_stddevs[i] += np.std(map(float, outs[:-1]))
+            lock_tps[i] += float(outs[-1]) / 2000
+            lock_stddevs[i] += np.std(map(lambda x: float(x) / 2000, outs[:-1]))
     
     lock_tps /= 3
     lock_stddevs /= 3
@@ -122,17 +122,111 @@ def counter_fairness():
     np.savetxt('results/counter_fairness_stds.csv', lock_stddevs, delimiter=',')
     return (lock_tps, lock_stddevs)
 
+def packet_overhead():
+    works = [25, 50, 100, 200, 400, 800]
+    locks_here = locks + ['noop']
+
+    packet_tps = np.zeros((len(works), len(locks_here)), dtype='float64')
+
+    for (w_ind, l_ind) in product(range(len(works)), range(len(locks_here))):
+        for rep in range(3):
+            print('packet_overhead: work {0} lock {1} rep {2}'.format(works[w_ind], locks_here[l_ind], rep))
+            out = timeout_output(['./perf_main', 'parallel_dispatcher', '2000', '1', '0', str(works[w_ind]), str(rep), '1', locks_here[l_ind]], 4)
+            packet_tps[w_ind, l_ind] += float(out) / 2000
+
+    packet_tps /= 3
+    np.savetxt('results/packet_overhead.csv', packet_tps, delimiter=',')
+    return packet_tps
+
+def uniform_speedup():
+    works = [1000, 2000, 4000, 8000]
+    srcs = [1, 3, 7, 15]
+    n_reps = 3
+
+    lockfree_tps = np.zeros((len(works), len(srcs)), dtype='float64')
+    for (w_ind, s_ind) in product(range(len(works)), range(len(srcs))):
+        for rep in range(n_reps):
+            print('uniform speedup lockfree work {0} source {1} rep {2}'.format(works[w_ind], srcs[s_ind], rep))
+            out = timeout_output(['./perf_main', 'parallel_dispatcher', '2000', str(srcs[s_ind]), '0', str(works[w_ind]), str(rep), '1', 'noop'], 4)
+            lockfree_tps[w_ind, s_ind] += float(out) / 2000
+
+    lockfree_tps /= n_reps
+
+    random_tps = np.zeros((len(works), len(srcs), len(locks)), dtype='float64')
+    home_tps = np.zeros((len(works), len(srcs), len(locks)), dtype='float64')
+
+    for (w_ind, s_ind, l_ind) in product(range(len(works)), range(len(srcs)), range(len(locks))):
+        for rep in range(n_reps):
+            print('uniform speedup random work {0} source {1} lock {2} rep {3}'.format(works[w_ind], srcs[s_ind], locks[l_ind], rep))
+            out = timeout_output(['./perf_main', 'parallel_dispatcher', '2000', str(srcs[s_ind]), '1', str(works[w_ind]), str(rep), '1', locks[l_ind]], 4)
+            random_tps[w_ind, s_ind, l_ind] += float(out) / 2000
+
+            print('uniform speedup homequeue work {0} source {1} lock {2} rep {3}'.format(works[w_ind], srcs[s_ind], locks[l_ind], rep))
+            out = timeout_output(['./perf_main', 'parallel_dispatcher', '2000', str(srcs[s_ind]), '2', str(works[w_ind]), str(rep), '1', locks[l_ind]], 4)
+            home_tps[w_ind, s_ind, l_ind] += float(out) / 2000
+
+    random_tps /= n_reps
+    home_tps /= n_reps
+
+    np.save('results/unif/lockfree', lockfree_tps)
+    np.save('results/unif/random', random_tps)
+    np.save('results/unif/home', home_tps)
+    return (lockfree_tps, random_tps, home_tps)
+
+def exp_speedup():
+    works = [1000]#, 2000, 4000, 8000]
+    srcs = [1, 3]#, 7, 15]
+    n_reps = 1
+
+    lockfree_tps = np.zeros((len(works), len(srcs)), dtype='float64')
+    for (w_ind, s_ind) in product(range(len(works)), range(len(srcs))):
+        for rep in range(n_reps):
+            print('exp speedup lockfree work {0} source {1} rep {2}'.format(works[w_ind], srcs[s_ind], rep))
+            out = timeout_output(['./perf_main', 'parallel_dispatcher', '2000', str(srcs[s_ind]), '0', str(works[w_ind]), str(rep), '0', 'noop'], 4)
+            lockfree_tps[w_ind, s_ind] += float(out) / 2000
+
+    lockfree_tps /= n_reps
+
+    random_tps = np.zeros((len(works), len(srcs), len(locks)), dtype='float64')
+    home_tps = np.zeros((len(works), len(srcs), len(locks)), dtype='float64')
+
+    for (w_ind, s_ind, l_ind) in product(range(len(works)), range(len(srcs)), range(len(locks))):
+        for rep in range(n_reps):
+            print('exp speedup random work {0} source {1} lock {2} rep {3}'.format(works[w_ind], srcs[s_ind], locks[l_ind], rep))
+            out = timeout_output(['./perf_main', 'parallel_dispatcher', '2000', str(srcs[s_ind]), '1', str(works[w_ind]), str(rep), '0', locks[l_ind]], 4)
+            random_tps[w_ind, s_ind, l_ind] += float(out) / 2000
+
+            print('exp speedup homequeue work {0} source {1} lock {2} rep {3}'.format(works[w_ind], srcs[s_ind], locks[l_ind], rep))
+            out = timeout_output(['./perf_main', 'parallel_dispatcher', '2000', str(srcs[s_ind]), '2', str(works[w_ind]), str(rep), '0', locks[l_ind]], 4)
+            home_tps[w_ind, s_ind, l_ind] += float(out) / 2000
+
+    random_tps /= n_reps
+    home_tps /= n_reps
+
+    np.save('results/exp/lockfree', lockfree_tps)
+    np.save('results/exp/random', random_tps)
+    np.save('results/exp/home', home_tps)
+    return (lockfree_tps, random_tps, home_tps)
 
 if __name__ == '__main__':
     os.mkdir('results')
+    os.mkdir('results/exp')
+    os.mkdir('results/unif')
+
     global logfile
     logfile = open('/tmp/hallliu_log', 'w', 0)
 
+    '''
     counter_overhead_work()
     counter_overhead_time()
 
     counter_scaling_time()
     counter_scaling_work()
+    '''
 
     counter_fairness()
+    packet_overhead()
+    uniform_speedup()
+    exp_speedup()
+    
     logfile.close()
