0f28e5f9ce14d10e00aab8327e75da5894e0de0f
Hall Liu <hallliu000@gmail.com>
2014-03-02 04:19:07 -0600

    not converging...

---
+++ stat310/hw8_files/p2.m
@@ -0,0 +1,134 @@
+
+function [x, iters, fevals] = p2(f, x0, rho, c_val, m, epsilon)
+    import java.util.LinkedList;
+    x = x0;
+    iters = 0;
+    fevals = 0;
+    records = LinkedList();
+    B = eye(length(x));
+    H = eye(length(x));
+    
+    [fx, gx] = f(x, 1);
+    while 1
+        if norm(gx) < epsilon
+            break
+        end
+        %norm(gx)
+        iters = iters + 1;
+        
+        %pk = -compute_Hv(records, gx);
+        pk = -H * gx;
+        
+        % Backtracking to find the step size ak
+        ak = 1;
+        f_val = f(x + ak * pk, 0);
+        fevals = fevals + 1;
+        while f_val > fx + c_val * ak * (gx.' * pk)
+            ak = rho * ak;
+            f_val = f(x + ak * pk, 0);
+            fevals = fevals + 1;
+        end
+        old_gx = gx;
+        x = x + ak * pk;
+        [fx, gx] = f(x, 1);
+        
+        % Damped bgfs update
+        sk = ak * pk;
+        yk = gx - old_gx;
+        %bksk = compute_Bv(records, sk);
+        bksk = B * sk;
+        
+        skyk = sk.' * yk;
+        sbs = sk.' * bksk;
+        if skyk >= 0.2 * sbs
+            theta = 1;
+        else
+            theta = 0.8 * sbs / (sbs - skyk);
+        end
+        rk = theta * yk + (1-theta) * bksk;
+        records.add({sk, rk, 1/skyk, bksk});
+        if records.size() > m
+            records.remove();
+        end
+        [B, H] = update_bgfs(B, H, sk, yk);
+        
+    end
+end
+
+% The evaluation function for H_k. It expects records to be stored in a cell array
+% in the following order:
+% 1: s_k
+% 2: r_k
+% 3: rho_k
+% 4: B_ks_k
+% all this is collected into a linked list.
+function hv = compute_Hv(records, v)
+    if records.size() == 0
+        hv = v;
+        return;
+    end
+    import java.util.LinkedList;
+    q = v;
+    
+    up = records.iterator();
+    down = records.descendingIterator();
+    
+    alphas = LinkedList();
+    
+    while down.hasNext()
+        data = cell(down.next());
+        alpha = data{3} * (data{1}.' * q);
+        q = q - alpha * data{2};
+        alphas.push(alpha);
+    end
+    
+    last_data = cell(records.getLast());
+    gamma = (last_data{1}.' * last_data{2}) / (last_data{2}.' * last_data{2});
+    r = q;
+    
+    while up.hasNext()
+        data = cell(up.next());
+        beta = data{3} * (data{2}.' * r);
+        r = r + data{1} * (alphas.pop() - beta);
+    end
+    hv = r;
+end
+
+function bv = compute_Bv(records, v)
+    if records.size() == 0
+        bv = v;
+        return
+    end
+    
+    last_data = cell(records.getLast());
+    gamma = (last_data{1}.' * last_data{2}) / (last_data{2}.' * last_data{2});
+    bv = v;
+    
+    it = records.iterator();
+    while it.hasNext()
+        data = cell(it.next());
+        sk = data{1};
+        rk = data{2};
+        ck = data{4};
+        bv = bv - (ck * (ck.' * v)) / (sk.' * ck) + (rk * (rk.' * v)) / (sk.' * rk);
+    end
+end
+
+function [B1, H1] = update_bgfs(B, H, s, y)
+    skyk = s.' * y;
+    bs = B * s;
+    sbs = s.' * bs;
+    
+    if skyk < 0.2 * sbs
+        theta = (0.8 * sbs) / (sbs - skyk);
+        r = theta * y - (1 - theta) * bs;
+    else
+        r = y;
+    end
+    
+    rho = 1/ (s.' * y);
+    H1 = H - rho * ((H * y) * s.' + s * (y.' * H)) + rho^2 * s * (y.' * H * y) * s.';
+    H1 = H1 + rho * (s * s.');
+   
+    B1 = B - (bs * bs.') / sbs + (r * r.') / (r.' * s);
+end
