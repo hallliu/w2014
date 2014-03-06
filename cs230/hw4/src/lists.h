8738c5d2af3ee0c0845da23ebb6f9b8f49449a37
Hall Liu <hallliu000@gmail.com>
2014-03-05 23:11:54 -0600

    stub for lock free list

--- cs230/hw4/src/lists.h
+++ cs230/hw4/src/lists.h
@@ -4,7 +4,9 @@
 
 #include <packetsource.h>
 
-//struct lockfree_list;
+struct lockfree_list {
+    int FIXME;
+};
 
 struct serial_list {
     volatile struct list_elem *head;
@@ -12,10 +14,15 @@ struct serial_list {
 };
 
 struct serial_list *create_serial_list(void);
-int s_add (struct serial_list *, int, Packet_t);
+int s_add (struct serial_list *, int, Packet_t *);
 bool s_remove (struct serial_list *, int);
 bool s_contains (struct serial_list *, int);
 void destroy_serial_list (struct serial_list *);
 
-//struct lockfree_list *create_lockfree_list(void);
+struct lockfree_list *create_lockfree_list(void);
+int lf_add (struct lockfree_list *, int, Packet_t *);
+bool lf_remove (struct lockfree_list *, int);
+bool lf_contains (struct lockfree_list *, int);
+void destroy_lockfree_list (struct lockfree_list *);
+
 #endif
