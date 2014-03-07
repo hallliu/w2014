52e86159f2b468841bacef208f1c7a5d729fcd05
Hall Liu <hallliu000@gmail.com>
2014-03-06 23:34:03 -0600

    first parallel test

--- cs230/hw4/tests/test_lists.c
+++ cs230/hw4/tests/test_lists.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits.h>
+#include <pthread.h>
 
 #include "../src/serial_lists.h"
+#include "../src/lockfree_lists.h"
+
+int *key_helper(int N, int limit);
 
 void serial_contains (int N);
 void serial_removals(int N);
@@ -34,6 +39,7 @@ int main(int argc, char *argv[]) {
     printf("Invalid test\n");
     return 1;
 }
+
 
 // Inserts N distinct random elements into a list, then tests that they're still all there.
 void serial_contains (int N) {
@@ -152,4 +158,112 @@ void serial_ordering (int N) {
 end:
     destroy_serial_list (l);
     free(keys);
+}
+
+// Parallel tests start here --------------------------------------
+
+// Makes an array of N distinct random values with upper bound limit. Uses
+// the (presumably correct) serial list to guarantee distinctness. 
+int *key_helper(int N, int limit) {
+    int *keys = malloc (N * sizeof(int));
+    srand (time(0));
+    struct serial_list *l = create_serial_list();
+
+    for (int i = 0; i < N; i++) {
+        keys[i] = rand() % limit;
+        while (!s_add (l, keys[i], NULL)) {
+            keys[i] = rand() % limit;
+        }
+    }
+    destroy_serial_list (l);
+    return keys;
+}
+
+// Parallel worker -- takes a struct with a array of keys, 
+// a array of operations (0=contains, 1=add, 2=remove), a begin
+// and an end (both inclusive), and performs
+// the requested operations, then stores the results in the
+// preallocated results array 
+typedef struct parallel_worker_data {
+    struct lockfree_list *l;
+    int *keys;
+    int *ops;
+    int begin;
+    int end;
+    bool *results;
+} pdata;
+
+void *parallel_worker(void *_data) {
+    pdata *data = (pdata) data;
+    struct lockfree_list *l = data->l;
+    int *keys = data->keys;
+        
+    for (int i = begin; i <= end; i++) {
+        switch (data->ops[i]) {
+            case 0:
+                data->results[i] = lf_contains(l, keys[i]);
+                break;
+            case 1:
+                data->results[i] = lf_add(l, keys[i], NULL);
+                break;
+            default:
+                data->results[i] = lf_remove(l, keys[i]);
+                break;
+        }
+    }
+    return NULL;
+}
+
+// Adds in N distinct keys to the list using T threads, then uses T threads
+// to test that they're in the list.
+void parallel_addcontain(int N, int T) {
+    int *keys = key_helper(N, INT_MAX);
+    bool *results = malloc(N * sizeof(bool));
+    int *ops = malloc(N * sizeof(int));
+    for (int i = 0; i < N; i++) 
+        ops[i] = 1;
+
+    if (T > N)
+        T = N;
+
+    struct lockfree_list *l = create_lockfree_list();
+
+    pdata *datas = malloc (T * sizeof(pdata));
+    pthread_t *threads = malloc(T * sizeof(pthread_t));
+    for (int i = 0; i < T; i++) {
+        datas[i].l = l;
+        datas[i].keys = keys;
+        datas[i].ops = ops;
+        datas[i].begin = i * N / T;
+        datas[i].end = (i + 1) * N / T - 1;
+        datas[i].results = results;
+    }
+
+    for (int i = 0; i < T; i++) 
+        pthread_create(&threads[i], NULL, parallel_worker, (void *)(datas + i));
+
+    for (int i = 0; i < T; i++) 
+        pthread_join(threads[i], NULL);
+
+    for (int i = 0; i < N; i++) {
+        if (!results[i]) {
+            printf("FAIL: bad return on parallel adds\n");
+            return;
+        }
+        ops[i] = 0; //convert it over to a containment operation
+    }
+
+    for (int i = 0; i < T; i++) 
+        pthread_create(&threads[i], NULL, parallel_worker, (void *)(datas + i));
+
+    for (int i = 0; i < T; i++) 
+        pthread_join(threads[i], NULL);
+
+    for (int i = 0; i < N; i++) {
+        if (!results[i]) {
+            printf("FAIL: bad return on parallel contains\n");
+            return;
+        }
+    }
+    return;
 }
