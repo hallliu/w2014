fa045167809dd3333e770141f9a999366281ed0f
Hall Liu <hallliu000@gmail.com>
2014-03-06 22:55:30 -0600

    lockfree list implemented

--- cs230/hw4/src/lockfree_lists.c
+++ cs230/hw4/src/lockfree_lists.c
@@ -4,9 +4,11 @@
 
 #include <packetsource.h>
 
-#include "lists.h"
+#include "lockfree_lists.h"
 
 #define _UNUSED_ __attribute__((unused))
+#define MARKOF(x) ((x) & (0x1))
+#define REFOF(x) ((x) & (~0x1))
 // Lookup table for bit-reversals copy-pasted from http://stackoverflow.com/a/746203
 static char reverses[256] = {  0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0, 
       0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8, 
@@ -34,21 +36,156 @@ static inline unsigned int rev_bits(int x) {
 }
 
 struct lockfree_list *create_lockfree_list(void) {
-    return NULL;
+    struct lockfree_list *l = malloc (sizeof(struct lockfree_list));
+    l->head = NULL;
+    l->size = 0;
+    return l;
 }
 
-int lf_add(struct lockfree_list *l _UNUSED_, int key _UNUSED_, Packet_t *pkt _UNUSED_) {
-    return 0;
+// Puts a pointer to the previous element in _prev.
+// If the key is in the list, then _curr will contain the address of the element.
+// If the key is not in the list, then _curr will contain the address of the element afterwards.
+// If _prev is unset but _curr is, then that indicates that we found the key at the head or that it should go at the head.
+// If _prev is set but _curr isn't, then that indicates that we didn't find the key and it should go at the tail
+// If both are unset, that means that the list is currently empty.
+void lf_find(volatile struct lockfree_list *l, unsigned rev_key, struct lf_elem **_prev, struct lf_elem **_curr) {
+    struct lf_elem *prev = NULL, *curr = NULL, *next = NULL;
+    while (1) {
+restart_find:
+
+        prev = l->head;
+        if (prev == NULL)
+            // This means list is empty. Do nothing to the two elements.
+            return;
+
+        curr = prev->next;
+
+        // Do some logic to remove the marked elements in the beginning
+        while (MARKOF(curr)) {
+            bool successful_phy_rm = __sync_bool_compare_and_swap (&l->head, prev, REFOF(curr));
+            if (!successful_phy_rm)
+                goto restart_find;
+            prev = REFOF(curr);
+            curr = curr->next;
+        }
+
+        if (prev == NULL)
+            return;
+
+        if (rev_key <= prev->rev_key) {
+            *_curr = REFOF(prev);
+            return;
+        }
+
+        while (1) {
+            if (curr == NULL) {
+                *_prev = REFOF(prev);
+                return;
+            }
+            next = curr->next;
+            while (MARKOF(next)) {
+                bool successful_phy_rm = __sync_bool_compare_and_swap (&prev->next, curr, next & (~0x1));
+                if (!successful_phy_rm)
+                    goto restart_find;
+                curr = REFOF(next);
+                next = curr->next;
+            }
+
+            if (rev_key < curr->rev_key) {
+                *_prev = REFOF(prev)
+                *_curr = REFOF(curr);
+                return;
+            }
+            prev = REFOF(prev);
+            curr = REFOF(curr);
+        }
+    }
 }
 
-bool lf_remove (struct lockfree_list *l _UNUSED_, int key _UNUSED_) {
-    return false;
+bool lf_add(struct lockfree_list *l, int key, Packet_t *pkt) {
+    unsigned rev_key = rev_bits(key);
+    struct lf_elem *prev = NULL, *curr = NULL;
+    // Allocate memory for this new elem now
+    struct lf_elem *new_elem = malloc (sizeof(struct lf_elem));
+    new_elem->pkt = pkt;
+    new_elem->key = key;
+    new_elem->rev_key = rev_key;
+
+    while (1) {
+        lf_find (l, rev_key, &prev, &curr);
+        if (prev == NULL) {
+            new_elem->next = curr;
+            if (curr == NULL) {
+                if (__sync_bool_compare_and_swap(&l->head, curr, new_elem))
+                    return true;
+                continue;
+            }
+            if (curr ->rev_key == rev_key) {
+                free (new_elem);
+                return false;
+            }
+            if (__sync_bool_compare_and_swap(&l->head, curr, new_elem))
+                return true;
+            continue;
+        }
+
+        if (curr && curr->rev_key == rev_key) {
+            free (new_elem);
+            return false;
+        }
+
+        new_elem->next = curr;
+        if (__sync_bool_compare_and_swap(&prev->next, curr, new_elem)) {
+            __sync_add_and_fetch (&l->size, 1);
+            return true;
+        }
+    }
 }
 
-bool lf_contains (struct lockfree_list *l _UNUSED_, int key _UNUSED_) {
-    return false;
+bool lf_remove (struct lockfree_list *l, int key) {
+    unsigned rev_key = rev_bits (key);
+    struct lf_elem *prev = NULL, *curr = NULL, *next = NULL;
+    while (1) {
+        lf_find (l, rev_key, &prev, &curr);
+        if (curr) {
+            if (curr->rev_key != rev_key) {
+                return false;
+            }
+            
+            next = curr->next;
+            bool successful_phy_rm = __sync_bool_compare_and_swap (&curr->next, next, next | 0x1);
+            if (!successful_phy_rm)
+                continue;
+            // Try a physical removal; no consequence if it fails.
+            if (prev)
+                __sync_bool_compare_and_swap (&prev->next, curr, REFOF(next));
+            else
+                __sync_bool_compare_and_swap (&l->head, curr, REFOF(next));
+            return true;
+        }
+        return false;
+    }
 }
 
-void destroy_lockfree_list (struct lockfree_list *l _UNUSED_) {
-    return;
+bool lf_contains (struct lockfree_list *l, int key) {
+    unsigned key = rev_bits(key);
+    struct lf_elem *curr = l->head;
+    if (!curr)
+        return false;
+    int marked = 0;
+    while (curr && rev_key > curr->rev_key) {
+        marked = MARKOF(curr->next);
+        curr = REFOF(curr->next);
+    }
+    return (curr && curr->rev_key == rev_key && !marked);
+}
+
+void destroy_lockfree_list (struct lockfree_list *l) {
+    struct lf_elem *curr = l->head, *next = NULL;
+    while (curr) {
+        next = REFOF(curr->next);
+        free (REFOF(curr));
+        curr = next;
+    }
+    free (l);
 }
--- cs230/hw4/src/lockfree_lists.h
+++ cs230/hw4/src/lockfree_lists.h
@@ -4,12 +4,24 @@
 
 #include <packetsource.h>
 
-struct lockfree_list {
-    int FIXME;
+struct lf_elem {
+    Packet_t *pkt;
+    unsigned key;
+    unsigned rev_key;
+    // We're going to use the LSB of this pointer as a mark.
+    // It seems likely that malloc would align to word boundaries
+    // which means that we'll get at least two zeros to play with.
+    struct lf_elem *next;
 };
 
+struct lockfree_list {
+    struct lf_elem *head;
+    volatile int size;
+};
+
+
 struct lockfree_list *create_lockfree_list(void);
-int lf_add (struct lockfree_list *, int, Packet_t *);
+bool lf_add (struct lockfree_list *, int, Packet_t *);
 bool lf_remove (struct lockfree_list *, int);
 bool lf_contains (struct lockfree_list *, int);
 void destroy_lockfree_list (struct lockfree_list *);
