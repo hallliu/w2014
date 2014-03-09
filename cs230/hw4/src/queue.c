91c479cdfe7fd32ba2895b9f196ecaf4259577cf
Hall Liu <hallliu000@gmail.com>
2014-03-09 18:49:13 -0500

    moved queues over

---
+++ cs230/hw4/src/queue.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "queue.h"
+
+
+struct l_queue *create_queues(int n_queues, int size) {
+    struct l_queue *q = calloc (n_queues, sizeof(struct l_queue));
+    for (int i = 0; i < n_queues; i++) {
+        q[i].items = calloc (size, sizeof(void *));
+        q[i].length = (unsigned) size;
+        q[i].queue_id = i;
+    }
+    return q;
+}
+
+void destroy_queues(int n_queues, struct l_queue *q) {
+    for (int i = 0; i < n_queues; i++) {
+        free (q[i].items);
+    }
+    free (q);
+}
+
+int enq(struct l_queue *q, void *obj) {
+    int head = q->head;
+    int tail = q->tail;
+    int len = q->length;
+
+    if (tail - head == len) {
+        return 1;
+    }
+
+    q->items[tail & (len - 1)] = obj;
+    q->n_enqueues += 1;
+    q->tail = tail + 1;
+    return 0;
+}
+
+int deq(struct l_queue *q, void **obj_ptr) {
+    int head = q->head;
+    int tail = q->tail;
+    int len = q->length;
+
+    if (tail == head) {
+    *obj_ptr = q->items[head & (len - 1)];
+    q->head = head + 1;
+    return 0;
+}
+
+int check_free (struct l_queue *q) {
+    int head = q->head;
+    int tail = q->tail;
+    int len = q->length;
+  
+    if (tail - head == len) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
+int get_n_enqueues (struct l_queue *q) {
+    return (q->n_enqueues);
+}
---
+++ cs230/hw4/src/queue.h
@@ -0,0 +1,19 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+struct l_queue {
+    volatile int head, tail;
+    unsigned length;
+    void **items;
+    int n_enqueues;
+    int queue_id;
+};
+
+struct l_queue *create_queues(int, int, char *, void *);
+void destroy_queues(int, struct l_queue *);
+int enq(struct l_queue *, void *);
+int deq(struct l_queue *, void **);
+int check_free(struct l_queue *);
+int get_n_enqueues(struct l_queue *);
+
+#endif
