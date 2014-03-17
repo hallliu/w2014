aee478c330e2fc759808731036e83c1d44692f2e
Hall Liu <hallliu000@gmail.com>
2014-03-17 16:40:03 -0500

    cauchy point code nominally working

--- stat310/final_files/p2/grad_proj.m
+++ stat310/final_files/p2/grad_proj.m
@@ -1,21 +1,27 @@
-function [ inds, bp_list] = grad_proj(ti)
-    import java.util.LinkedList;
-    [inds, bp_list] = get_active_bnds(ti);
+function [ inds, bp_list] = grad_proj(x, g, lower_bnds, G, c)
+    [inds, bp_list] = compute_cauchy_point(x, g, lower_bnds, G, c);
 end
 
 function [cp, active_set] = compute_cauchy_point(x, g, lower_bnds, G, c)
-    bps = compute_breakpoints(x, g, lower_bnds);
+    bps = compute_breakpoints(x, g, lower_bnds).';
     [inds, bp_list] = get_active_bnds(bps);
     active_set = false(1, length(g));
-    for i = 1:length(bp_list)
+    for i = 1:(length(bp_list) - 1)
         this_ind_activations = inds(i);
-        while (this_ind_activations.hasNext())
+        while (~this_ind_activations.isEmpty())
             active_set(this_ind_activations.pop()) = true;
         end
         cp = find_local_min(x, g, bps, bp_list(i), bp_list(i+1), active_set, G, c);
         if ~isnan(cp)
             break;
         end
+    end
+    
+    % At this point, if cp still has a NaN in it, it means that the cauchy
+    % point is at a corner of the constraints.
+    if any(isnan(cp))
+        cp = x - bps .* g;
+        active_set = true(1, length(g));
     end
 end
 
@@ -33,20 +39,22 @@ function cp = find_local_min(x, g, bps, t, t1, actives, G, c)
     
     % Compute the location of the path for this breakpoint
     xt = x - t * g;
-    clamped_xt = x - bps * g;
+    clamped_xt = x - bps .* g;
     xt(actives) = clamped_xt(actives);
     
     % Compute the two necessary coefficients
     f1 = c.' * p + xt.' * G * p;
     f2 = p.' * G * p;
     
-    if f1 < 0
-        cp = t;
+    if f1 > 0
+        tcp = t;
     elseif -f1 / f2 < t1 - t
-        cp = t - f1 / f2;
+        tcp = t - f1 / f2;
     else
-        cp = NaN;
+        tcp = NaN;
     end
+    cp = x - tcp * g;
+    cp(actives) = clamped_xt(actives);
 end
 
 % Computes which components of the gradient become active at any particular
@@ -55,13 +63,17 @@ end
 % entry the components that become active on the subinterval starting
 % at that entry in bp_list.
 function [inds, bp_list] = get_active_bnds(ti)
+    import java.util.LinkedList;
     [bp_list, ~, ic] = unique(ti);
-    bp_list = [0 bp_list];
-    inds = javaArray('java.util.LinkedList', length(bp_list) - 1);
+    bp_list = [0; bp_list];
+    inds = javaArray('java.util.LinkedList', length(bp_list));
+    for i = 1:length(inds)
+        inds(i) = LinkedList();
+    end
     
-    for i = 1:(length(bp_list) - 1)
+    for i = 1:length(ti)
         i2 = ic(i);
-        inds(i2).push(i);
+        inds(i2+1).push(i);
     end
     
 end
