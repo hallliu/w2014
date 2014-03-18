7dbc75a8e00dd8208414e88d134a6e11a156c66c
Hall Liu <hallliu000@gmail.com>
2014-03-17 23:15:24 -0500

    p3

--- stat310/final_files/p2/grad_proj_opt.m
+++ stat310/final_files/p2/grad_proj_opt.m
@@ -1,9 +1,13 @@
 function x = grad_proj_opt(G, c, lower_bnds, start, epsilon, pc)
     x = start;
     gx = G*x + c;
-    
-    while norm(gx) > epsilon
+    actives = false(length(gx), 1);
+    while 1
         [cp, actives] = compute_cauchy_point(x, gx, lower_bnds, G, c);
+        norm(gx(~actives))
+        if norm(gx(~actives)) < epsilon
+            break
+        end
         [x, cg_iters] = bounded_pcg_reduced(G, c, cp, actives, lower_bnds, pc);
         gx = G*x + c;
     end
---
+++ stat310/final_files/p3/p3_int_point.m
@@ -0,0 +1,12 @@
+% Parameters: 
+% start is some arbitrary starting point
+% epsilon is the stopping tolerance
+% X and y are the data matrix and label vector
+
+ 
+ 
+function x = p3_int_point(start, epsilon, X, y)
+    
+
+end
+
